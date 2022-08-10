5fd417866ea4f24ad197aeab7dda8b71ccc76813
kill_in_sun <noneck.0311@gmail.com>
2022-08-11 00:56:11 +0900

    updated

--- install.sh
+++ install.sh
@@ -16,12 +16,17 @@ if [ ! -e ~/.vim ]; then
 	mkdir ~/.vim
 fi
 
+if [ ! -e ~/bin ]; then
+	mkdir ~/bin
+fi
+
 unlink ~/.gitconfig
 unlink ~/.vimrc
 unlink ~/.vim/colors;
 unlink ~/.vim/ftdetect;
 unlink ~/.vim/indent;
 unlink ~/.tmux.conf
+unlink ~/bin/tmux-alias.sh
 
 ln -sf $SCRIPT_DIR/.gitconfig ~/.gitconfig
 ln -sf $SCRIPT_DIR/.vimrc ~/.vimrc
@@ -29,6 +34,7 @@ ln -sf $SCRIPT_DIR/.vim/colors  ~/.vim/colors
 ln -sf $SCRIPT_DIR/.vim/ftdetect ~/.vim/ftdetect
 ln -sf $SCRIPT_DIR/.vim/indent ~/.vim/indent
 ln -sf $SCRIPT_DIR/.tmux.conf ~/.tmux.conf
+ln -sf $SCRIPT_DIR/tmux-alias.sh ~/bin/tmux-alias.sh
 
 
 if [ $OS == 'Mac' ]; then
